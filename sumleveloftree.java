c94f52e955293913f4bf174c2cebe55ade70c8f3
Tirth Patel <tirthpatel@Tirths-MacBook-Air.local>
2026-01-06 22:10:38 +0530

    adding a sum of level of binary tree

---
+++ sumleveloftree.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+sumleveloftreeTreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        int maxLevel = 1;
+        int currentLevel = 1;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            int currentLevelSum = 0;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                currentLevelSum += node.val;
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            if (currentLevelSum > maxSum) {
+                maxSum = currentLevelSum;
+                maxLevel = currentLevel;
+            }
+
+            currentLevel++;
+        }
+
+        return maxLevel;
+    }
+}
+
+   public static void main(String[] args) {
+    // Creating Example 1: [1, 7, 0, 7, -8]
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(7);
+    root.right = new TreeNode(0);
+    root.left.left = new TreeNode(7);
+    root.left.right = new TreeNode(-8);
+
+    Solution sol = new Solution();
+    System.out.println("The level with the maximum sum is: " + sol.maxLevelSum(root));
+}
