4824b2550e9c9ec79d25008c85f1b3c9c02b9a06
tirth707 <ptirth2206@gmail.com>
2026-01-09 22:39:46 +0530

    smallest subtree node problem

---
+++ Smallest Subtree node.java
@@ -0,0 +1,62 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    class Result {
+        TreeNode node;
+        int dist;
+
+        Result(TreeNode node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+    }
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode node) {
+        if (node == null) {
+            return new Result(null, 0);
+        }
+
+        Result left = dfs(node.left);
+        Result right = dfs(node.right);
+
+        if (left.dist > right.dist) {
+            return new Result(left.node, left.dist + 1);
+        }
+        if (right.dist > left.dist) {
+            return new Result(right.node, right.dist + 1);
+        }
+        return new Result(node, left.dist + 1);
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        TreeNode result = sol.subtreeWithAllDeepest(root);
+        System.out.println("Root of the smallest subtree: " + result.val);
+    }
+}
